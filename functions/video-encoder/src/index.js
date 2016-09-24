eac4ba7613d29a7fedd141fd0841ca33a9951a75
Ehsan Afzali <eafzali@gmail.com>
2016-09-24 14:09:29 +0300

    add source to video config

--- functions/video-encoder/src/index.js
+++ functions/video-encoder/src/index.js
@@ -111,7 +111,7 @@ async function processVideo(event) {
   console.log('Downloading new images', images.length);
   await Promise.all(images.map((image, i) => downloadObject({
     bucket: event.bucket,
-    key: config.source ? `${event.cam}/${config.source}/${image}.jpg` : `full/${event.cam}/${image}.jpg`,
+    key: config.video.source ? `${event.cam}/${config.video.source}/${image}.jpg` : `full/${event.cam}/${image}.jpg`,
     dest: `${tmpDir}/${zp(i, 3)}.jpg`,
   })));
 
