ac32746dc235ce8cac7246882627516be8d3873d
Matt Margolin <matt.margolin@gmail.com>
2016-12-19 17:22:27 -0800

    update function closure

--- functions/video-handler/src/index.js
+++ functions/video-handler/src/index.js
@@ -1,13 +1,12 @@
 import λ from 'apex.js';
 import AWS from 'aws-sdk';
 
-var ec2 = new AWS.EC2({
-  apiVersion: '2016-11-15',
-  region    : "us-east-1"
-});
-
-
 async function handleEC2Action(action, instance_ids) {
+
+  var ec2 = new AWS.EC2({
+    apiVersion: '2016-11-15',
+    region    : "us-east-1"
+  });
 
   switch (action) {
     case "start":
@@ -25,5 +24,5 @@ async function handleEC2Action(action, instance_ids) {
 }
 
 // Lambda function entry point
-export default λ(event => handleEC2Action(event.Records[0].action, event.Records[0].instance_id));
+export default λ(event => handleEC2Action(event.action, event.instance_ids));
 
